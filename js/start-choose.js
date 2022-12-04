987f3a2b77c2365fcae681a85174aad0e43758bc
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-04 22:27:53 +0000

    Create login based on actor

---
+++ js/start-choose.js
@@ -0,0 +1,40 @@
+var chosen = 'none';
+
+function goToLogin(chosenActor) {
+    console.log(chosenActor);
+    chosen = chosenActor;
+    $('#actor').addClass('d-none');
+    $('#login').removeClass('d-none');
+}
+
+function backToActor() {
+    console.log('back to actor')
+    chosen = 'none';
+    $('#login').addClass('d-none');
+    $('#actor').removeClass('d-none');
+}
+
+function login(chosen) {
+    console.log('login');
+    if (chosen === 'none') {
+        alert('Please choose an actor');
+        return;
+    }
+    window.location.href = chosen + '/index.html';
+}
+
+$('#clientButton').click(function() {
+    goToLogin('client');
+});
+
+$('#ptButton').click(function() {
+    goToLogin('pt');
+});
+
+$('#goBackButton').click(function() {
+    backToActor();
+});
+
+$('#loginButton').click(function() {
+    login(chosen);
+});
