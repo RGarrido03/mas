5e59192ac412c902f4aea361f49c16c74e0d1cf8
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-11 23:07:03 +0000

    Fetch exercise data from JSON, add progress circle

--- client/js/fetchData.js
+++ client/js/fetchData.js
@@ -35,4 +35,56 @@ $(document).ready(function() {
       '</div>')
     });
   });
+
+    $.getJSON('json/exercise.json', function(data) {
+        var doneCount = 0;
+        var lengthCount = 0;
+        $.each(data, function(key, val) {
+            lengthCount++;
+            if (val.done == true) {
+                doneCount++;
+                $("#exercise-Plan").html($("#exercise-Plan").html() +
+                '<p class="mt-3 mb-0"><span class="fw-semibold text-success">' + key + 'º dia</span>&nbsp; ' + val.focus + '</p>')
+            }
+            else if (val.actual == true) {
+                var locationIcon = (val.isOnline == true) ? 'play' : 'building';
+                $("#exercise-Plan").html($("#exercise-Plan").html() +
+                '<div class="row">'+
+                  '<div class="col">'+
+                    '<p class="dark-gray mt-3 mb-0">'+
+                      '<span class="fw-semibold dark-gray">' + key + 'º dia</span>&nbsp;' + val.focus + '<br>'+
+                      val.duration + ' minutos | ' + val.calories + ' kcal | ' + val.difficulty +
+                    '</p>'+
+                  '</div>'+
+                  '<div class="col-auto">'+
+                    '<div class="float-end">'+
+                      '<i class="fa-solid fa-' + locationIcon + ' dark-gray mt-3 mb-0"></i>'+
+                    '</div>'+
+                  '</div>'+
+                '</div>')
+            }
+            else {
+                $("#exercise-Plan").html($("#exercise-Plan").html() +
+                '<p class="mt-3 mb-0"><span class="fw-semibold">' + key + 'º dia</span>&nbsp; ' + val.focus + '</p>')
+            }
+        });
+        
+        $("#exercise-workoutInsight-SVGtext").text(doneCount + '/' + lengthCount);
+        
+        var ratio = doneCount / lengthCount;
+        if (ratio == 1) {
+            $("#exercise-workoutInsight-content").text("Tudo feito por esta semana.")
+            $("#exercise-workoutInsight-title").text("Parabéns!")
+        }
+        else if (ratio > 0.5) {
+            $("#exercise-workoutInsight-content").text("Metade já está!")
+            $("#exercise-workoutInsight-title").text("Continua o bom trabalho.")
+        }
+        else {
+            $("#exercise-workoutInsight-content").text("O esforço é o primeiro passo.")
+            $("#exercise-workoutInsight-title").text("Vamos lá!")
+        }
+
+        $("#exercise-workoutInsight-SVGpath").attr('stroke-dasharray', ratio * 222.2222 + ", 222.2222")
+    })
 });
