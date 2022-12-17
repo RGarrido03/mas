b3f2a5498276fbc966f586eb7232dc38e3a3df31
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-17 11:20:28 +0000

    404 page: Use "Go back button", set title & theme-color

--- src/notFound.jsx
+++ src/notFound.jsx
@@ -1,17 +1,25 @@
-import { Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Row, Col, Button } from 'react-bootstrap';
+import { Helmet } from 'react-helmet';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
-    const svgStyle = { height: "4rem", width: '4rem', fill: "#0066CC" };
+
+
+    const svgStyle = { height: "5rem", width: '5rem', fill: "#0066CC" };
+    const navigate = useNavigate();
     return (
         <div className='text-center'>
-            <Row className='align-items-center w-100 m-0' style={{height:'100vh'}}>
+            <Helmet>
+                <title>404 - Não encontrado</title>
+                <meta name="theme-color" content="#FFFFFF" />
+            </Helmet>
+            <Row className='align-items-center w-100 m-0' style={{ height: '100vh' }}>
                 <Col>
                     <svg style={svgStyle} className="mb-5" xmlns="http://www.w3.org/2000/svg" width="139" height="139" viewBox="0 0 139 139">
                         <path d="M69.5 139C107.793 139 139 107.856 139 69.5C139 31.2066 107.856 0 69.5 0C31.1444 0 0 31.2066 0 69.5C0 107.856 31.2066 139 69.5 139ZM69.5 136.576C64.8998 136.576 60.4861 136.14 56.1346 135.208L56.8184 133.343L69.4378 100.955L81.9951 132.97L82.5546 134.462L82.8654 135.208C78.576 136.14 74.1002 136.576 69.5 136.576ZM69.5 2.42441C106.488 2.42441 136.576 32.5121 136.576 69.5C136.576 90.263 127.064 108.912 112.207 121.221L111.585 119.667L69.5 10.195L27.1038 120.413L26.7929 121.221C11.8734 108.912 2.36225 90.263 2.36225 69.5C2.42441 32.5121 32.5121 2.42441 69.5 2.42441Z" />
                     </svg>
                     <h1 className='mb-3'>Oops! Erro 404.</h1>
-                    <Link to="/" type="button" className="btn primary-blue-background white">Voltar ao início</Link>
+                    <Button type="button" className="btn primary-blue-background white" onClick={() => navigate(-1)}>Voltar atrás</Button>
                 </Col>
             </Row>
         </div>
