933d9571c69cd5e46691625ae7679d05f98193e3
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-17 00:23:26 +0000

    Transition to React: Add Client login & register pages

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ActorChoosing from './actorChoosing';
-import ClientLogin from './client/login';
-import PTLogin from './pt/login';
+import ClientLogin from './client/clientLogin';
+import ClientRegister1 from './client/clientRegister1';
+import ClientRegister2 from './client/clientRegister2';
+import ClientRegister3 from './client/clientRegister3';
+import NotFound from './notFound';
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -23,7 +26,10 @@ root.render(
       <Routes>
         <Route path="/" element={<ActorChoosing />} />
         <Route path="/client/login" element={<ClientLogin />} />
-        <Route path="/pt/login" element={<PTLogin />} />
+        <Route path="/client/register/1" element={<ClientRegister1 />} />
+        <Route path="/client/register/2" element={<ClientRegister2 />} />
+        <Route path="/client/register/3" element={<ClientRegister3 />} />
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </Router>
   </React.StrictMode>
