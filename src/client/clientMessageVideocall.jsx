e1e6c4859c6e5eee53044944a2e8dd65c599a5d0
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-18 11:33:09 +0000

    Client -> Messages -> Chat -> Videocall: make button clickable

--- src/client/clientMessageVideocall.jsx
+++ src/client/clientMessageVideocall.jsx
@@ -40,7 +40,7 @@ function ClientExerciseVideocall() {
                         </Col>
                         <Col></Col>
                         <Col xs="auto" className="ps-0">
-                            <div className="bg-danger rounded-circle d-flex align-items-center justify-content-center" style={{ width: '3rem', height: '3rem' }} onClick={() => navigate(-1)}>
+                            <div className="bg-danger rounded-circle d-flex align-items-center justify-content-center clickable" style={{ width: '3rem', height: '3rem' }} onClick={() => navigate(-1)}>
                                 <i className='fa-solid fa-phone-hangup white fa-lg'></i>
                             </div>
                         </Col>
