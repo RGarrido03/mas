155fd181618a1f89659b39c932695e05db451805
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-26 21:22:57 +0000

    Client (all) -> Subscription: Fetch data from API

--- src/client-standard/clientStandardSubscription.jsx
+++ src/client-standard/clientStandardSubscription.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../navbar/navbarBack';
 import { Container, Card, Button } from 'react-bootstrap';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Data from '../api/client/subscription_standard.json';
 
 function ClientStandardSubscription() {
     return (
@@ -17,14 +18,10 @@ function ClientStandardSubscription() {
                         <div className="text-start">
                             <Card.Title className='mb-0 bold'>Plano atual</Card.Title>
                             <Card.Text className='mt-2 mb-2'>
-                                <span className="bold">Premium</span><br />
-                                - Gestão de planos de treino<br />
-                                - Medidas corporais<br />
-                                - Planos de treino criados por um PT<br />
-                                - Mensagens e videochamadas com um PT<br />
-                                - Planos de nutrição criados por um PT<br />
+                                <span className="bold">{Data.plan.name}</span><br />
+                                <span>{Data.plan.description.split('\n').map((value, index) => { return <span key={index}>{value}<br /></span> })}</span>
                             </Card.Text>
-                            <Card.Text className='mt-0 semibold'>24,99€/mês</Card.Text>
+                            <Card.Text className='mt-0 semibold'>{Data.plan.price}€/mês</Card.Text>
                         </div>
                         <Button className='btn white mt-3 primary-blue-background'><i className='fa-regular fa-repeat white'></i>&nbsp;&nbsp;Trocar de plano</Button>
                     </Card.Body>
@@ -34,7 +31,7 @@ function ClientStandardSubscription() {
                         <div className="text-start">
                             <Card.Title className='mb-0 bold'>Método de pagamento</Card.Title>
                             <Card.Text className='mt-2'>
-                                <i className='fa-brands fa-cc-visa'></i>&nbsp;&nbsp;Cartão de débito (acabado em 0595)
+                                <i className={Data.payment.icon}></i>&nbsp;&nbsp;{Data.payment.name}
                             </Card.Text>
                         </div>
                         <Button className='btn white mt-3 primary-blue-background'><i className='fa-regular fa-repeat white'></i>&nbsp;&nbsp;Trocar método</Button>
@@ -45,7 +42,7 @@ function ClientStandardSubscription() {
                         <div className="text-start">
                             <Card.Title className='mb-0 bold'>Gestão da subscrição</Card.Title>
                             <Card.Text className='mt-2'>
-                                Subscrição mensal. A próxima renovação ocorrerá a 15 de janeiro (24,99€).
+                                Subscrição {Data.info.type}. A próxima renovação ocorrerá a {Data.info.date} ({Data.info.price}€).
                             </Card.Text>
                         </div>
                         <Button className='btn btn-danger white mt-3'><i className='fa-regular fa-xmark white'></i>&nbsp;&nbsp;Cancelar subscrição</Button>
