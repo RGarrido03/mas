e7da75d2fd5a95f21df7913404a7bf06995244b4
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-27 00:18:57 +0000

    Show app version on login footer

--- src/footer/footerLogin.jsx
+++ src/footer/footerLogin.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Modal from '../modal/modalLogin';
+import packageInfo from '../../package.json';
+const { version } = packageInfo;
 
 function FooterLogin() {
   const [show, setShow] = useState(false);
@@ -11,7 +13,7 @@ function FooterLogin() {
     <div className="py-3 border-top off-white-background fixed-bottom">
       <Modal show={show} handleClose={handleClose} handleShow={handleShow}/>
       <Container fluid className="px-3">
-        <p className="float-start mb-0">P3 - Grupo 4</p>
+        <p className="float-start mb-0">v{version}</p>
         <div className="float-end">
           <a className="me-3" href="https://github.com/RGarrido03/MAS"><i className="fa-lg fa-brands fa-github"></i></a>
           <span className="clickable" onClick={()=> handleShow()}><i className="fa-lg fa-regular fa-circle-question"></i></span>
